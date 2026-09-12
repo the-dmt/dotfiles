4aea16f82911bfef860df8ddaf2b8d51a0266a92
the-dmt <diontaylor87@proton.me>
2026-09-12 10:31:45 +0930

    modify macos.zsh

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -87,8 +87,5 @@ unset __conda_setup
 source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
 chruby ruby-3.4.1
 
-eval "$(starship init zsh)"
-
-export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
